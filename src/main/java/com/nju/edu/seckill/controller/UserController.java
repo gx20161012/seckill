0ae2e95f92f3a544c3344c0587dd85b9bc07fcc8
gaoxiang <gaoxiang199703@hotmail.com>
2019-07-31 00:57:24 +0800

    add files

--- src/main/java/com/nju/edu/seckill/controller/UserController.java
+++ src/main/java/com/nju/edu/seckill/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.nju.edu.seckill.controller;
 
 import com.nju.edu.seckill.domain.User;
+import com.nju.edu.seckill.redis.RedisService;
 import com.nju.edu.seckill.result.Result;
 import com.nju.edu.seckill.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,15 +18,27 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    RedisService redisService;
+
     @RequestMapping("/user/{id}")
     @ResponseBody
     public Result<User> getById(@PathVariable("id") Integer id){
         return Result.success(userService.getById(id));
     }
+
+
     @ResponseBody
     @RequestMapping("/user/tx")
     public Result<Boolean> f(){
         userService.tx();
         return Result.success(true);
+    }
+
+    @ResponseBody
+    @RequestMapping("/redis/get")
+    public Result<String> g(){
+        String v1 = redisService.get("k1");
+        return Result.success(v1);
     }
 }
--- src/test/java/com/nju/edu/seckill/SeckillApplicationTests.java
+++ src/test/java/com/nju/edu/seckill/SeckillApplicationTests.java
@@ -2,15 +2,21 @@ package com.nju.edu.seckill;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SeckillApplicationTests {
 
+    @Autowired
+    RedisTemplate redisTemplate;
+
     @Test
     public void contextLoads() {
+        System.out.println(redisTemplate.opsForValue().get("k1"));
     }
 
 }
