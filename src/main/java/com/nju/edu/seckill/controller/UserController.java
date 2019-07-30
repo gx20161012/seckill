64aa26e42a2a989ea17029fe8a2536d145e1cd06
gaoxiang <gaoxiang199703@hotmail.com>
2019-07-30 19:11:20 +0800

    add

--- src/main/java/com/nju/edu/seckill/controller/UserController.java
+++ src/main/java/com/nju/edu/seckill/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.nju.edu.seckill.controller;
 
+import com.nju.edu.seckill.domain.User;
+import com.nju.edu.seckill.result.Result;
 import com.nju.edu.seckill.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,8 +19,13 @@ public class UserController {
 
     @RequestMapping("/user/{id}")
     @ResponseBody
-    public String getById(@PathVariable("id") Integer id){
-        return userService.getById(id).toString();
+    public Result<User> getById(@PathVariable("id") Integer id){
+        return Result.success(userService.getById(id));
     }
-
+    @ResponseBody
+    @RequestMapping("/user/tx")
+    public Result<Boolean> f(){
+        userService.tx();
+        return Result.success(true);
+    }
 }
--- src/main/java/com/nju/edu/seckill/mapper/UserMapper.java
+++ src/main/java/com/nju/edu/seckill/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.nju.edu.seckill.mapper;
 
 import com.nju.edu.seckill.domain.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -9,4 +10,7 @@ import org.apache.ibatis.annotations.Select;
 public interface UserMapper {
     @Select("select * from user where id = #{id}")
     public User getById(@Param("id") Integer id);
+
+    @Insert("insert into user (id, name) values (#{id}, #{name})")
+    public int insert(User user);
 }
